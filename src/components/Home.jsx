4f71dd48d197178a7a5170c2ba70c04eee63fcdd
qericho <echstmr@gmail.com>
2025-08-04 17:58:51 +0800

    updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-scroll';
 import { motion } from 'framer-motion';
 import { fadeIn } from '../variants';
 import { IoMdArrowDown } from "react-icons/io";
-import Profile from '../assets/img/displaypic.png'
+import Profile from '../assets/img/displaypic.jpg'
 
 const Home = () => {
   const [overlay, setOverlay] = useState('bottom-[-50%]');
@@ -19,33 +19,33 @@ const Home = () => {
     <div className='relative h-dvh w-full flex items-center' id='home'>
       {/* Overlay */}
       <div className={`h-1/2 w-full bg-[#1a324c] absolute transition-all duration-500 ${overlay}`}></div>
-      <motion.div 
-      variants={fadeIn("", 1.7)}
-      initial="hidden"
-      whileInView="show"
-      viewport={{ once: true }}
-      className='hover:size-60 size-15 duration-500 absolute top-10 right-10 hidden xl:flex '>
-          <img src={Profile} className='opacity-70 hover:opacity-100 duration-500 rounded-full object-cover' alt="" />
+      <motion.div
+        variants={fadeIn("", 1.7)}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true }}
+        className='hover:size-60 size-15 duration-500 absolute top-10 right-10 hidden xl:flex '>
+        <img src={Profile} className='opacity-70 hover:opacity-100 duration-500 rounded-full object-cover' alt="" />
       </motion.div>
       {/* Hero Content */}
-      <div 
+      <div
         className='relative top-4 z-10 w-full px-3 md:px-20'>
-        <motion.p 
-           variants={fadeIn("right", 0)}
-           initial="hidden"
-           whileInView="show"
-           viewport={{ once: true }}
-         
-        className='text-xl md:text-2xl text-[#8cb0d9]'>I'm Echo</motion.p>
+        <motion.p
+          variants={fadeIn("right", 0)}
+          initial="hidden"
+          whileInView="show"
+          viewport={{ once: true }}
+
+          className='text-xl md:text-2xl text-[#8cb0d9]'>I'm Echo</motion.p>
         <motion.h1
           variants={fadeIn("up", 0.2)}
           initial="hidden"
           whileInView="show"
           viewport={{ once: true }}
-         className='text-[35px] md:text-5xl lg:text-6xl text-[#f7fafc] font-[800] my-2'>
+          className='text-[35px] md:text-5xl lg:text-6xl text-[#f7fafc] font-[800] my-2'>
           Front-End Developer
         </motion.h1>
-        <motion.p 
+        <motion.p
           variants={fadeIn("up", 0.4)}
           initial="hidden"
           whileInView="show"
@@ -54,18 +54,18 @@ const Home = () => {
           I build websites with a focus on responsive design and accessibility.
         </motion.p>
       </div>
-      
-      <motion.button 
+
+      <motion.button
         variants={fadeIn("up", 0.6)}
         initial="hidden"
         whileInView="show"
         viewport={{ once: true }}
         className='flex items-center gap-1 absolute text-[#8cb0d9] bottom-8 left-3 md:left-20 text-[18px] group cursor-pointer'
       >
-        <IoMdArrowDown className='animate-bounce group-hover:text-[#f7fafc]'/> 
+        <IoMdArrowDown className='animate-bounce group-hover:text-[#f7fafc]' />
         <span className='group-hover:text-[#f7fafc] text-[18px]'>
-            <Link to="about" smooth={true} duration={500}>scroll</Link>
-          </span>
+          <Link to="about" smooth={true} duration={500}>scroll</Link>
+        </span>
       </motion.button>
     </div>
   );
